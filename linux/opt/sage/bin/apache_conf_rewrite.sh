56c6d8bb432e8747565b41e52892dd9ec492ed2c
Khai Do <zaro0508@gmail.com>
2020-10-21 09:58:20 -0700

    [SC-241] refactor apache script
    remove apache scripts dependence on make_env_vars_file.sh which
    will make the script simpler and make provisioning instances
    faster.

--- linux/opt/sage/bin/apache_conf_rewrite.sh
+++ linux/opt/sage/bin/apache_conf_rewrite.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-. /opt/sage/bin/instance_env_vars.sh  && sed -i "s/^.*<LocationMatch.*\/.*\/>.*$/<LocationMatch \/$EC2_INSTANCE_ID\/>/g" /etc/apache2/sites-available/proxy.conf
+EC2_INSTANCE_ID=$(/usr/bin/curl -s http://169.254.169.254/latest/meta-data/instance-id)
+sed -i "s/^.*<LocationMatch.*\/.*\/>.*$/<LocationMatch \/$EC2_INSTANCE_ID\/>/g" /etc/apache2/sites-available/proxy.conf
 systemctl restart apache2
