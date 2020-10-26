97925c4961e778f6589d2608f019fd9748e7b9b3
Khai Do <3697686+zaro0508@users.noreply.github.com>
2020-10-26 08:20:25 -0700

    [SC-241] refactor apache script (#16)
    remove apache scripts dependence on make_env_vars_file.sh which
    will make the script simpler and make provisioning instances
    faster.

--- linux/opt/sage/bin/apache_conf_rewrite.sh
+++ linux/opt/sage/bin/apache_conf_rewrite.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-. /opt/sage/bin/instance_env_vars.sh  && sed -i "s/^.*<LocationMatch.*\/.*\/>.*$/<LocationMatch \/$EC2_INSTANCE_ID\/>/g" /etc/apache2/sites-available/proxy.conf
+
+# This script adds a route in the Apache config for web requests with the URI that identifies the
+# notebook. The path of this request gets rewritten by the proxy so that information identifying the
+# notebook gets stripped out before the traffic is received by the notebook web server.
+# We did it this way because the Apache module we use for rewrite did not support regular expressions.
+
+EC2_INSTANCE_ID=$(/usr/bin/curl -s http://169.254.169.254/latest/meta-data/instance-id)
+sed -i "s/^.*<LocationMatch.*\/.*\/>.*$/<LocationMatch \/$EC2_INSTANCE_ID\/>/g" /etc/apache2/sites-available/proxy.conf
 systemctl restart apache2
