02e3e8901af52cb3afb817f37dd411b348787378
Khai Do <zaro0508@gmail.com>
2023-06-16 10:21:52 -0700

    [SC-383] Update path to aws cli
    We updated the AWS CLI when we moved to using ubuntu 22.0[1]
    as the base image for the service catalog EC2 products.
    The AWS CLI v2 is installed into `/usr/local/bin/aws` instead
    of `/usr/bin/aws`.
    
    [1] https://github.com/Sage-Bionetworks-IT/packer-base-ubuntu-jammy

--- linux/opt/sage/bin/make_env_vars_file.sh
+++ linux/opt/sage/bin/make_env_vars_file.sh
@@ -17,7 +17,7 @@ if [[ -z "$AWS_REGION" || -z "$STACK_ID" || -z "$STACK_NAME" ]]; then
 fi
 
 EC2_INSTANCE_ID=$(/usr/bin/curl -s http://169.254.169.254/latest/meta-data/instance-id)
-ROOT_DISK_ID=$(/usr/bin/aws ec2 describe-volumes \
+ROOT_DISK_ID=$(/usr/local/bin/aws ec2 describe-volumes \
   --region "$AWS_REGION" \
   --filters Name=attachment.instance-id,Values="$EC2_INSTANCE_ID" \
   --query Volumes[].VolumeId --out text)
@@ -35,7 +35,7 @@ PROVISIONING_PRINCIPAL_ARN=$(extract_tag_value 'aws:servicecatalog:provisioningP
 PRINCIPAL_ID=${PROVISIONING_PRINCIPAL_ARN##*/}  #Immutable Synapse userid derived from assume-role session name
 ASSUMED_ROLE_NAME=$(cut -d'/' -f2 <<< ${PROVISIONING_PRINCIPAL_ARN}) # the SC end user assumed role name
 # the SC end user assumed role ID
-ACCESS_APPROVED_ROLEID=$(/usr/bin/aws --region $AWS_REGION \
+ACCESS_APPROVED_ROLEID=$(/usr/local/bin/aws --region $AWS_REGION \
   iam get-role --query Role.RoleId --out text --role-name ${ASSUMED_ROLE_NAME})
 
 if [[ "$PRINCIPAL_ID" =~ [[:digit:]] ]]; then
@@ -54,7 +54,7 @@ else
 fi
 
 RESOURCE_ID=${STACK_ID##*/}
-PRODUCTS=$(/usr/bin/aws --region $AWS_REGION \
+PRODUCTS=$(/usr/local/bin/aws --region $AWS_REGION \
   servicecatalog search-provisioned-products \
   --filters SearchQuery=$RESOURCE_ID )
 NUM_PRODUCTS=$(echo $PRODUCTS | jq -r '.TotalResultsCount')
