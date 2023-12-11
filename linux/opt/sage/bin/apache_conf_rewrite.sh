4807e7357364abfd5db36e1b430efd068de6fc69
Joni Harker <joni.harker@sagebase.org>
2023-12-11 10:38:35 -0800

    Also update the other scripts

--- linux/opt/sage/bin/apache_conf_rewrite.sh
+++ linux/opt/sage/bin/apache_conf_rewrite.sh
@@ -5,6 +5,7 @@
 # notebook gets stripped out before the traffic is received by the notebook web server.
 # We did it this way because the Apache module we use for rewrite did not support regular expressions.
 
-EC2_INSTANCE_ID=$(/usr/bin/curl -s http://169.254.169.254/latest/meta-data/instance-id)
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+EC2_INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/instance-id)
 sed -i "s/EC2_INSTANCE_ID/$EC2_INSTANCE_ID/g" /etc/apache2/sites-available/proxy.conf
 systemctl restart apache2
